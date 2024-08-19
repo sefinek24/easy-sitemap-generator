c00670b2b3d7955dc3438f556102046eaa8492dc
Sefinek <contact@sefinek.net>
2024-08-19 04:37:45 +0200

    example.com

--- examples/main.js
+++ examples/main.js
@@ -1,7 +1,7 @@
 const sitemap = require('../lib/sitemapGenerator.js');
 
 (async () => {
-	const content = await sitemap.generate('https://nekosia.cat/documentation');
+	const content = await sitemap.generate('https://example.com');
 	console.log(content);
 	console.log('Module version:', sitemap.version);
 })();
