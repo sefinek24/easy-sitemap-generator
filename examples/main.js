85fbe9438fac7ba05af9293874c03775d7e5826c
Sefinek <contact@sefinek.net>
2024-08-19 04:36:44 +0200

    Bug fix

--- examples/main.js
+++ examples/main.js
@@ -1,7 +1,7 @@
 const sitemap = require('../lib/sitemapGenerator.js');
 
 (async () => {
-	const content = await sitemap.generate('https://example.com');
+	const content = await sitemap.generate('https://nekosia.cat/documentation');
 	console.log(content);
 	console.log('Module version:', sitemap.version);
 })();
